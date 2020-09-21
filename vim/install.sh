feaf0ae8041fa9313602e9d545a3ad48e536dba5
cyliu <cyliu@localhost.localdomain>
2020-09-21 15:10:05 +0800

    remove cd in vim/install scripts

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+script_dir="$(dirname "$0")"
 mkdir -p ~/.vim/bundle
+rm -rf ~/.vim/bundle/tagbar
 cd ~/.vim/bundle/ && git clone git://github.com/majutsushi/tagbar ; cd -
-cp ./vimrc ~/.vimrc
+
+cp ${script_dir}/vimrc ~/.vimrc
 echo "call pathogen#infect() " >> ~/.vimrc
 echo "call pathogen#helptags()" >> ~/.vimrc
 mkdir -p ~/.vim/autoload
-cp ./pathogen.vim ~/.vim/autoload
+cp ${script_dir}/pathogen.vim ~/.vim/autoload
+echo "Done"  
